0922f7d1d5d21f0a15d7e5b85c00b2ecef186e7a
Phillip <philliplee515@gmail.com>
2022-07-27 15:34:17 +0900

    pointer

---
+++ main_pointer.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	a:= 2
+	b:= &a
+	a = 10
+	*b = 20
+	
+	
+	fmt.Println(a, b)
+	fmt.Println(&a, b)
+	fmt.Println("*b = ", *b)
+	fmt.Println("a = ", a)
+	fmt.Println(b, *b, &b)
+}
